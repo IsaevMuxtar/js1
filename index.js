8aab9825e4397aea29bae301b537e315d17265a0
Mukhtar <ismandiyarov.muxtar@mail.ru>
2022-06-01 00:30:10 +0600

    add to ex4

---
+++ index.js
@@ -0,0 +1,21 @@
+// ex1
+/* let name = "Джон";
+let admin = "Джон";
+alert (admin)*/
+//ex2 
+let earth = "";
+let user = "";
+//ex3
+/*
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+*/
+//ex4
+let name = prompt('Ваше имя');
+
+alert(`Привет ${name} !`);
