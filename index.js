e20316173b04fdc69a4406dcf8ab07d118001df5
Mukhtar <ismandiyarov.muxtar@mail.ru>
2022-06-01 21:16:26 +0600

    add to ex13

--- index.js
+++ index.js
@@ -3,19 +3,77 @@
 let admin = "Джон";
 alert (admin)*/
 //ex2 
-let earth = "";
-let user = "";
+/*let earth ="";
+let user ="";*/
 //ex3
-/*
-let name = "Ilya";
-
-alert( `hello ${1}` ); // ?
-
-alert( `hello ${"name"}` ); // ?
-
-alert( `hello ${name}` ); // ?
+/*let name = "Ilya";
+alert( `hello ${1}` ); номер // ?
+alert( `hello ${"name"}` ); строка // ?
+alert( `hello ${name}` ); переменны // ?
 */
 //ex4
-let name = prompt('Ваше имя');
+/*let name  = prompt('Имя');
+alert(`Привет ${name} !`);*/
+//ex5
+/*"" + 1 + 0      //= 10
+"" - 1 + 0      //=-1
+true + false    //=1
+6 / "3"	        //=2	
+"2" * "3"       //=6
+4 + 5 + "px"    //=9px
+"$" + 4 + 5     //=$45
+"4" - 2         //=2
+"4px" - 2       //=NaN
+"  -9  " + 5    //=-9 5
+"  -9  " - 5    //=-14
+null + 1        //=1
+undefined + 1   //=NaN
+" \t \n" - 2    //=-2*/
+//ex6
+/*let a = +prompt("Первое число?",1);
+let b = +prompt("Второе число?",2);
+alert(a+b)*/
+//ex7
+/*let num = 3;
+alert(`${num}`)*/
+//ex8
+/*let a = 10;
+let b = 2;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)*/
+//ex9
+/*let c=15;
+let d=2;
+result=c+d;
+console.log(result)*/
+//ex10
+/*let c= 5;
+a=10;
+b=2;
+result=(c+a+b)
+console.log(result)*/
+//ex11
+/*let a=17;
+b=10;
+d=7;
+c = a-b;
+result= c+d;
+console.log(result)*/
+//ex12
+/*let a = 5;
+result = Math.pow(a, 2);
+console.log(result);*/
+//ex13
+/*let day = 15;
+hours = 26;
+month = 12;
 
-alert(`Привет ${name} !`);
+result_day = day * 24 * 3600;
+result_hours = hours * 3600;
+result_month = month * 30 * 24 * 3600;
+
+console.log(day+" "+"суток"+" "+result_day + " "+ "секунд");
+console.log(hours+" "+"часа"+" "+result_hours + " "+ "секунд");  
+console.log(month+" "+"месяца"+" "+result_month + " "+ "секунд");*/
